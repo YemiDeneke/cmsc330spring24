d6b9360bf9f0cf2e4ab010c28d637244a66f5e17
YemiDeneke <152354848+YemiDeneke@users.noreply.github.com>
2024-04-07 02:30:09 -0400

    Update lexer.ml

--- discussions/d8_parsing/src/lexer.ml
+++ discussions/d8_parsing/src/lexer.ml
@@ -21,6 +21,16 @@ match lst with
 | h::[] -> conv h
 | h::t -> (conv h) ^ " " ^ (string_of_list conv t)
 
+let strint = Str.regexp "[0-9]+"
 (* Given source code returns a token list. *)
-let rec lexer (input : string) : token list =
-  failwith "unimplemented"
+let rec lexer (input : string) : token list = 
+  let rec tok pos s = 
+    if pos >= (String.length s) then [Tok_EOF] 
+    else if (Str.string_match (Str.regexp "*") s pos) then 
+      Tok_Mult::(tok (pos + 1) s)
+    else if (Str.string_match strint s pos) then
+      let matched = Str.matched_string s in
+      (Tok_int (int_of_string matched))::(tok (pos + 1) s)
+    else if Str.string_match (Str.regexp "+") s pos then
+      Tok_Add::(tok (pos+1) s
+    else some error
